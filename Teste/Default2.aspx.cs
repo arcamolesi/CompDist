76fbf9be1aba8c48d70f8290512572f2d35a923c
Camolesi <Camolesi@CAMOLESI-DELL-5>
2016-08-17 17:00:39 -0300

    Teste Completo

---
+++ Teste/Default2.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        localService.ServicosSoma srvSomar = new localService.ServicosSoma();
+        int v1 = Convert.ToInt32(TextBox1.Text);
+        int v2 = Convert.ToInt32(TextBox2.Text);
+        int sm = srvSomar.Somar(v1, v2);
+        Label1.Text = sm.ToString(); 
+    }
+}
---
+++ webSrvSoma/App_Code/ServicosSoma.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for ServicosSoma
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class ServicosSoma : System.Web.Services.WebService
+{
+
+    public ServicosSoma()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld()
+    {
+        return "Hello World";
+    }
+
+    [WebMethod]
+    public int Somar(int val1, int val2)
+    {
+        int soma = val1 + val2;
+        return soma; 
+    }
+}
